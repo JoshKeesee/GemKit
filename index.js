ee4d4409d83eab42b046b01e36ed1db4236ef8e4
JoshKeesee <105324950+JoshKeesee@users.noreply.github.com>
2023-11-08 23:35:54 +0000

    .

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const server = require("http").createServer(app);
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(server);
 require("ejs");
+const fs = require("fs");
+const minify = require("@node-minify/core");
+const clean = {
+	"CSS": require("@node-minify/clean-css"),
+	"JS": require("@node-minify/terser"),
+};
 
-const appName = process.env.REPL_SLUG || "GemKit";
+const appName = process.env.REPL_SLUG || "Gemkit";
+const gameModes = [];
 const path = {
 	css: "css/",
 	js: "js/",
-	icon: "icon.png",
+	icon: "images/icon.png",
 };
 
 app.set("views", "./public/pages");
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-	const p = "index";
-	res.render(p, {
-		css: path.css + p + ".css",
-		js: path.js + p + ".js",
-		icon: path.icon,
-		appName,
+const render = (req, res) => {
+	let p = "index";
+	if (req.params.path != "undefined" && req.params.path != "" && req.params.path) p = req.params.path;
+	try {
+		res.sendFile(p);
+	} catch (e) {
+		try {
+			res.render(p, {
+				icon: path.icon,
+				appName,
+				gameModes,
+				header: req.body.header || true,
+		 });
+		} catch (e) {
+			p = "error";
+			res.render(p, {
+				icon: path.icon,
+				appName,
+				gameModes,
+				header: false,
+			});
+		}
+	}
+};
+
+app.get("/", render);
+app.get("/:path", render);
+app.post("/:path", render);
+
+const compile = (dir, output, type) => {
+	let compressed = "";
+	const p = "./public/" + dir;
+	const filenames = fs.readdirSync(p);
+	filenames.forEach(f => {
+		const c = fs.readFileSync(p + f, "utf8");
+		compressed += c;
 	});
-});
+	fs.writeFileSync("./public/" + output, compressed);
+	minify({
+		compressor: clean[type],
+		input: "./public/" + output,
+		output: "./public/" + output,
+		callback: () => console.log(type + " Minified"),
+	});
+};
+
+compile(path.css, "min.css", "CSS");
+compile(path.js, "min.js", "JS");
 
 io.on("connection", socket => {
 	console.log("New connection");
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,95 @@
-const join = document.querySelector("#join-game");
-const signUp = document.querySelector("#sign-up");
+let loading = false;
 
-join.onclick = () => window.location.href = "join";
-signUp.onclick = () => window.location.href = "signup";
+const redirect = async e => {
+	e.preventDefault();
+	if (loading) return;
+	loading = true;
+	const url = e.target.href;
+	const ti = setTimeout(() => window.location.href = url, 5000);
+	const data = await fetch(url, {
+		method: "POST",
+		body: JSON.stringify({ header: false }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	clearTimeout(ti);
+	document.body.innerHTML = await data.text();
+	document.querySelectorAll("a").forEach(e => e.onclick = redirect);
+	const p = (url || "/").replace(window.location.origin, "");
+	updateURL(p, true);
+	update();
+	loading = false;
+};
+
+const updateURL = (url, set) => {
+	if (set) window.history.pushState({}, "", url);
+	const p = url.replaceAll("/", "").replace("signup", "sign up");
+	const d = p.replace(/(^\w{1})|(\s+\w{1})/g, l => l.toUpperCase());
+	document.title = window.location.pathname == "/join" ? "Play " + appName + "! Enter game code here | " + appName : d ? d + " | " + appName : url.split("?")[0].endsWith("/") ? appName + " - live learning game show" : appName;
+}
+
+const validateEmail = e => {
+	return String(e)
+		.toLowerCase()
+		.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+
+const signupOrLogin = e => {
+	if (e.target.classList.contains("disabled")) return;
+	const em = document.querySelector("#email");
+	if (!validateEmail(em.value)) return;
+};
+
+const updateLogin = () => {
+	if (window.location.pathname != "/login" && window.location.pathname != "/signup") return;
+	const c = document.querySelector("#continue");
+	document.querySelector("#email").oninput = e => {
+		const em = validateEmail(e.target.value);
+		if (em) c.classList.remove("disabled");
+		else c.classList.add("disabled");
+	};
+	c.onclick = signupOrLogin;
+};
+
+let gamecode = false;
+
+const checkGamecodeOrUsername = e => {
+	const g = document.querySelector("#game-code");
+	if (g.value.length == 0) return;
+	const f = document.querySelector("#form");
+	if (gamecode) {
+		const username = g.value;
+	} else {
+		gamecode = g.value;
+		f.style.transform = "scale(0.95)";
+		f.style.opacity = 0;
+		setTimeout(() => {
+			g.value = "";
+			g.type = "text";
+			g.placeholder = "Name";
+			f.style = "";
+		}, 500);
+	}
+};
+
+const updateJoin = () => {
+	if (window.location.pathname != "/join") return;
+	gamecode = false;
+	document.querySelector("#join-game").onclick = checkGamecodeOrUsername;
+	document.querySelector("#game-code").onkeyup = e => e.key == "Enter" ? checkGamecodeOrUsername() : "";
+};
+
+const update = () => {
+	document.querySelectorAll("a").forEach(e => e.onclick = redirect);
+	updateLogin();
+	updateJoin();
+}
+
+window.onload = () => update();
+window.onpopstate = () => {
+	window.history.replaceState({}, "", document.referrer);
+	window.location = document.referrer;
+};
+
+updateURL(window.location.pathname, false);
